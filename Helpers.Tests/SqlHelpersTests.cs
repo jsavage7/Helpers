63a85212cf74077ab9777be05261292d1642d4ab
Jacob <jsavage7@outlook.com>
2023-03-01 22:37:25 -0500

    Added SQL helpers and tests.

---
+++ Helpers.Tests/SqlHelpersTests.cs
@@ -0,0 +1,73 @@
+﻿using Helpers.Sql;
+using Microsoft.Data.SqlClient;
+
+namespace Helpers.Tests;
+
+/*Table to test with:
+ *
+ *
+USE [Test]
+GO
+SET ANSI_NULLS ON
+    GO
+
+SET QUOTED_IDENTIFIER ON
+    GO
+
+CREATE TABLE [dbo].[Table_1](
+    [Test] [nvarchar](50) NULL
+    ) ON [PRIMARY]
+GO
+
+ */
+
+public class SqlHelpersTests
+{
+    private string ConnectionString { get; set; }
+    
+    public SqlHelpersTests()
+    {
+        ConnectionString =
+            "Server=localhost;Database=Test;Integrated Security=True;Encrypt=False;";
+    }
+    
+    [Test]
+    public void TableExists()
+    {
+        var sqlHelpers = new SqlHelpers(ConnectionString);
+        var result = sqlHelpers.TableExists("Table_1");
+        Assert.True(result);
+    }
+    
+    [Test]
+    public void ColumnExists()
+    {
+        var sqlHelpers = new SqlHelpers(ConnectionString);
+        var result = sqlHelpers.ColumnExists("Table_1", "Test");
+        Assert.True(result);
+    }
+    
+    [Test]
+    public void ColumnExistsWithSchema()
+    {
+        var sqlHelpers = new SqlHelpers(ConnectionString);
+        var result = sqlHelpers.ColumnExists("Table_1", "Test", "dbo");
+        Assert.True(result);
+    }
+    
+    [Test]
+    public void GetDataSet()
+    {
+        var sqlHelpers = new SqlHelpers(ConnectionString);
+        var result = sqlHelpers.GetDataSet("SELECT * FROM Table_1");
+        Assert.True(result.Tables.Count > 0);
+    }
+    
+    [Test]
+    public void GetDataSetWithParameters()
+    {
+        var sqlHelpers = new SqlHelpers(ConnectionString);
+        var result = sqlHelpers.GetDataSetWithParams("SELECT * FROM Table_1 WHERE Test = @test", new List<SqlParameter> { new SqlParameter("@test", "Test") });
+        Assert.True(result.Tables.Count > 0);
+    }
+}
---
+++ Helpers/Sql/SqlHelpers.cs
@@ -0,0 +1,115 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace Helpers.Sql;
+
+public class SqlHelpers
+{
+    private string ConnectionString { get; set; }
+    
+    public SqlHelpers(string connectionString)
+    {
+        ConnectionString = connectionString;
+    }
+    
+    public bool TableExists(string tableName)
+    {
+        using var connection = new SqlConnection(ConnectionString);
+        connection.Open();
+        
+        var cmd = new SqlCommand("SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = @tableName", connection);
+        cmd.Parameters.AddWithValue("@tableName", tableName);
+        
+        var count = (int)cmd.ExecuteScalar();
+        
+        return count > 0;
+    }
+    
+    public bool ColumnExists(string tableName, string columnName)
+    {
+        using var connection = new SqlConnection(ConnectionString);
+        connection.Open();
+        
+        var cmd = new SqlCommand("SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = @tableName AND COLUMN_NAME = @columnName", connection);
+        cmd.Parameters.AddWithValue("@tableName", tableName);
+        cmd.Parameters.AddWithValue("@columnName", columnName);
+        
+        var count = (int)cmd.ExecuteScalar();
+        
+        return count > 0;
+    }
+    
+    public bool ColumnExists(string tableName, string columnName, string schemaName)
+    {
+        using var connection = new SqlConnection(ConnectionString);
+        connection.Open();
+        
+        var cmd = new SqlCommand("SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = @tableName AND COLUMN_NAME = @columnName AND TABLE_SCHEMA = @schemaName", connection);
+        cmd.Parameters.AddWithValue("@tableName", tableName);
+        cmd.Parameters.AddWithValue("@columnName", columnName);
+        cmd.Parameters.AddWithValue("@schemaName", schemaName);
+        
+        var count = (int)cmd.ExecuteScalar();
+        
+        return count > 0;
+    }
+    
+    public DataSet GetDataSet(string sql)
+    {
+        using var connection = new SqlConnection(ConnectionString);
+        connection.Open();
+        
+        var cmd = new SqlCommand(sql, connection);
+        var da = new SqlDataAdapter(cmd);
+        var ds = new DataSet();
+        
+        da.Fill(ds);
+        
+        return ds;
+    }
+    
+    public DataSet GetDataSetWithParams(string sql, List<SqlParameter> parameters)
+    {
+        using var connection = new SqlConnection(ConnectionString);
+        connection.Open();
+        
+        var cmd = new SqlCommand(sql, connection);
+        
+        foreach (var parameter in parameters)
+        {
+            cmd.Parameters.Add(parameter);
+        }
+        
+        var da = new SqlDataAdapter(cmd);
+        var ds = new DataSet();
+        
+        da.Fill(ds);
+        
+        return ds;
+    }
+    
+    public int ExecuteNonQuery(string sql)
+    {
+        using var connection = new SqlConnection(ConnectionString);
+        connection.Open();
+        
+        var cmd = new SqlCommand(sql, connection);
+        
+        return cmd.ExecuteNonQuery();
+    }
+    
+    public int ExecuteNonQueryWithParams(string sql, List<SqlParameter> parameters)
+    {
+        using var connection = new SqlConnection(ConnectionString);
+        connection.Open();
+        
+        var cmd = new SqlCommand(sql, connection);
+        
+        foreach (var parameter in parameters)
+        {
+            cmd.Parameters.Add(parameter);
+        }
+        
+        return cmd.ExecuteNonQuery();
+    }
+}
